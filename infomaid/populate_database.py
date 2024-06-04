4929b79348d4318c88b76fd53f9db0192b1890c1
Oliver Bonham-Carter <obonhamcarter@allegheny.edu>
2024-06-04 19:57:38 -0400

    update deprecated code

--- infomaid/populate_database.py
+++ infomaid/populate_database.py
@@ -3,19 +3,28 @@
 
 # Parts of this code taken from reference: https://github.com/pixegami/rag-tutorial-v2
 
+
 import argparse
 import os
 import shutil
-from langchain.document_loaders.pdf import PyPDFDirectoryLoader
+# from langchain.document_loaders.pdf import PyPDFDirectoryLoader # deprecated code
+from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.schema.document import Document
 
+from langchain_community.document_loaders import UnstructuredXMLLoader
+from langchain_community.document_loaders import UnstructuredMarkdownLoader
+from langchain_core.documents import Document
+
+
 # from get_embedding_function import get_embedding_function
 from infomaid import get_embedding_function
-from langchain.vectorstores.chroma import Chroma
+# from langchain.vectorstores.chroma import Chroma # deprecated code
+from langchain_community.vectorstores import Chroma
 
 from rich.console import Console
 from infomaid import main
+
 
 CHROMA_PATH = "chroma"
 DATA_PATH = "data"
